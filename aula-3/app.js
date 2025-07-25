03214a135df6b1d3a1e1c6ad5ef4d183b29b2374
Arthur Caetano Da Silva <arthur.caetano.da.silva01@gmail.com>
2025-07-25 18:14:34 -0300

    Update app.js

--- aula-3/app.js
+++ aula-3/app.js
@@ -1,7 +1,19 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-let chute = prompt('Escolha um número de 1 a 10');
+console.log(numeroSecreto)
+let chute
 
-if (chute == numeroSecreto) {
-    console.log('Parabéns vc descobril o número secreto (5)')
+//enquanto não for igual ao numeroSecreto
+while(chute != numeroSecreto) {
+    chute = prompt('Escolha um número de 1 a 10');
+    //if e else --> se sim ou se não
+    if (chute == numeroSecreto) {
+        alert(`Parabéns, vc descobril o número secreto ${numeroSecreto}`)
+     } else {
+        if (chute > numeroSecreto) {
+            alert(`O númeor secreto é menor que ${chute}`);
+       } else {
+            alert(`O númeor secreto é maior que ${chute}`);
+       }
+    }
 }
